ef3953a51da61db7c261f15ecb1c15a81e7948b5
Chris Puppe <chrispuppe@gmail.com>
2022-04-30 16:19:21 -0700

    Setting up func to gather game data

--- access.py
+++ access.py
@@ -10,7 +10,7 @@ from nba_api.stats.static import teams
 from nba_api.stats.library.parameters import SeasonAll
 import numpy as np
 
-player_dict = players.get_players()
+
 
 # print(player_dict)
 # Use ternary operator or write function 
@@ -19,48 +19,68 @@ player_dict = players.get_players()
 # bron_id = bron['id']
 
 player_input_name = 'Stephen Curry'
-selected_player = [player for player in player_dict if player['full_name'] == f'{player_input_name}'][0]
-selected_player_id = selected_player['id']
+
 # print(selected_player_id)
 # print(bron['full_name'] + ' Player ID: ' + str(bron_id))
 
 # find team Ids
 
-teams = teams.get_teams()
-team_input_name = 'Golden State Warriors'
+# teams = teams.get_teams()
+# team_input_name = 'Golden State Warriors'
 # print(teams)
-GSW = [x for x in teams if x['full_name'] == team_input_name][0]
-GSW_id = GSW['id']
+# GSW = [x for x in teams if x['full_name'] == team_input_name][0]
+# GSW_id = GSW['id']
 
 # print(GSW['full_name'] + ' Team ID: ' + str(GSW_id))
 
 #Call the API endpoint passing in lebron's ID & which season
  
 selected_year = '2021'
-  
-gamelog_player_input = playergamelog.PlayerGameLog(player_id=f'{selected_player_id}', season_type_all_star='Playoffs', season=selected_year)
-
-
-# # print(gamelog_player_input.DataSet.player_game_log)
-# #Converts gamelog object into a pandas dataframe
-# #can also convert to JSON or dictionary  
-
-df_player_input = gamelog_player_input.get_data_frames()[0]
-
 finals_team_1 = 'GSW'
 finals_team_2 = 'DEN'
 
-number_of_games = len(df_player_input)
-for i in range(0, number_of_games):
-    game = df_player_input.loc[i,'MATCHUP']
-    if finals_team_1 in game and finals_team_2 in game:
-        if df_player_input.loc[i,'WL'] == 'W':
-            results = 'won'
-        else:
-            results = 'lost'
-        player_score = df_player_input.loc[i,'PTS']
-        game_day = df_player_input.loc[i,'GAME_DATE']
-        print(f'On {game_day} the {game} and the {team_input_name} {results}, {player_input_name} had {player_score}pts.')
+def get_player_finals_stats(player_name):
+    player_dict = players.get_players()
+    selected_player = [player for player in player_dict if player['full_name'] == f'{player_input_name}'][0]
+    selected_player_id = selected_player['id']
+    gamelog_player_input = playergamelog.PlayerGameLog(
+                                                        player_id=f'{selected_player_id}', 
+                                                        season_type_all_star='Playoffs', 
+                                                        season=selected_year)
+    df_player_input = gamelog_player_input.get_data_frames()[0]
+    number_of_games = len(df_player_input)
+    player_playoff_stats = []
+    for i in range(0, number_of_games):
+        game = df_player_input.loc[i,'MATCHUP']
+        if finals_team_1 in game and finals_team_2 in game:
+            player_pts = df_player_input.loc[i,'PTS']
+            player_reb = df_player_input.loc[i,'REB']
+            player_ast = df_player_input.loc[i,'AST']
+            player_stl = df_player_input.loc[i,'STL']
+            player_blk = df_player_input.loc[i,'BLK']
+            player_tot = (
+                player_pts
+                + player_reb * 2
+                + player_ast * 2
+                + player_stl * 3
+                + player_blk * 3
+            )
+            game_day = df_player_input.loc[i,'GAME_DATE']
+
+            player_game_stats = [
+                player_name,
+                player_pts,
+                player_reb,
+                player_ast,
+                player_stl,
+                player_blk,
+                player_tot,
+                game_day
+            ]
+            player_playoff_stats.append(player_game_stats)
+    return player_playoff_stats
+
+print(get_player_finals_stats(player_input_name))
 
 
 # labels = []
