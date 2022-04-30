1524b4aa716ee96896fa471766211d217d3e3294
Chris Puppe <chrispuppe@gmail.com>
2022-04-30 15:37:49 -0700

    API learning

--- access.py
+++ access.py
@@ -32,37 +32,41 @@ team_input_name = 'Golden State Warriors'
 GSW = [x for x in teams if x['full_name'] == team_input_name][0]
 GSW_id = GSW['id']
 
-print(GSW['full_name'] + ' Team ID: ' + str(GSW_id))
+# print(GSW['full_name'] + ' Team ID: ' + str(GSW_id))
 
 #Call the API endpoint passing in lebron's ID & which season
-# 
-# 
-#  
-gamelog_bron = playergamelog.PlayerGameLog(player_id=f'{selected_player_id}')
+ 
+selected_year = '2021'
+  
+gamelog_player_input = playergamelog.PlayerGameLog(player_id=f'{selected_player_id}', season_type_all_star='Playoffs', season=selected_year)
 
 
-# # print(gamelog_bron.DataSet.player_game_log)
+# # print(gamelog_player_input.DataSet.player_game_log)
 # #Converts gamelog object into a pandas dataframe
 # #can also convert to JSON or dictionary  
 
-df_bron_games_2018 = gamelog_bron.get_data_frames()[0]
+df_player_input = gamelog_player_input.get_data_frames()[0]
 
-number_of_games = len(df_bron_games_2018)
+finals_team_1 = 'GSW'
+finals_team_2 = 'DEN'
+
+number_of_games = len(df_player_input)
 for i in range(0, number_of_games):
-    game = df_bron_games_2018.loc[i,'MATCHUP']
-    if df_bron_games_2018.loc[i,'WL'] == 'W':
-        results = 'won'
-    else:
-        results = 'lost'
-    player_score = df_bron_games_2018.loc[i,'PTS']
-    game_day = df_bron_games_2018.loc[i,'GAME_DATE']
-    print(f'On {game_day} the {game} and the {team_input_name} {results}, {player_input_name} had {player_score}pts.')
+    game = df_player_input.loc[i,'MATCHUP']
+    if finals_team_1 in game and finals_team_2 in game:
+        if df_player_input.loc[i,'WL'] == 'W':
+            results = 'won'
+        else:
+            results = 'lost'
+        player_score = df_player_input.loc[i,'PTS']
+        game_day = df_player_input.loc[i,'GAME_DATE']
+        print(f'On {game_day} the {game} and the {team_input_name} {results}, {player_input_name} had {player_score}pts.')
 
 
 # labels = []
-# for game in df_bron_games_2018:
+# for game in df_player_input:
 #     labels.append(game)
-# df = pd.DataFrame(df_bron_games_2018, index=labels)
+# df = pd.DataFrame(df_player_input, index=labels)
 
 # print("Select specific columns:")
 # print(df[['GAME_DATE', 'MATCHUP']])
@@ -70,11 +74,11 @@ for i in range(0, number_of_games):
 # If you want all seasons, you must import the SeasonAll parameter 
 
 
-# gamelog_bron_all = playergamelog.PlayerGameLog(player_id='2544', season = SeasonAll.all)
+# gamelog_player_input_all = playergamelog.PlayerGameLog(player_id='2544', season = SeasonAll.all)
 
-# df_bron_games_all = gamelog_bron_all.get_data_frames()
+# df_bron_games_all = gamelog_player_input_all.get_data_frames()
 
-# for item in gamelog_bron:
+# for item in gamelog_player_input:
 #     print(item)
 
 #### Find players on specified team ####
