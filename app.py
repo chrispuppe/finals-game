6a5a0f91403fe2ec98176630ae41b7e09727fed5
Chris Puppe <chrispuppe@gmail.com>
2022-05-14 13:53:14 -0700

    Flask templates added and started setup

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+@app.route('/index')
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
