3c6a86fc2a952e312a846858d1fbef24e2b996a0
Chris Puppe <chrispuppe@gmail.com>
2022-05-18 15:49:54 -0700

    Started work on database setup and installed Flask-SQLAlchemy

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Programming/db/FinalsGame.db'
 db = SQLAlchemy(app)
 
 
