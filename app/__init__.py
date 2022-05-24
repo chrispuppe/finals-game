84c7fefaa44eb16f12fbfbbb7442dd3a18fe947a
Chris Puppe <chrispuppe@gmail.com>
2022-05-24 15:48:26 -0700

    Major refactoring to get db working and seperate concerns

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import secrets
 from flask_debugtoolbar import DebugToolbarExtension
-# from flask_bootstrap import Bootstrap
+
 # Create an Instance of Flask
 app = Flask(__name__)
 # Include config from config.py
 app.config.from_object('config')
-app.config['SECRET_KEY'] = secrets.token_urlsafe(24)
-# app.secret_key = 's4asdgfkjagh2345nnlqnexiIS9732KksdnsdklkLKJjlksdfJLDF02418'
+
 # Create an instance of SQLAclhemy
 db = SQLAlchemy(app)
-# Bootstrap(app)
+
 toolbar = DebugToolbarExtension(app)
 
 from app import views, models
--- setup-db.py
+++ setup-db.py
@@ -1,10 +1,17 @@
-# https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
-from app import db
 # from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
+# from app.models import User, Selection
+# from app import db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from flask_login import UserMixin
 
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Programming/db/FinalsGame.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class User(UserMixin, db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
@@ -28,3 +35,19 @@ class Selection(db.Model):
         self.game_date = game_date
         self.selected_player = selected_player
         self.user_selection_order = user_selection_order
+
+
+db.drop_all()
+db.create_all()
+
+chris = User(username='Chris', email='chrispuppe@gmail.com', password='passowrd')
+todd = User(username='Todd', email='tb@gmail.com', password='passowrd')
+anddrew = User(username='Andrew', email='as@gmail.com', password='passowrd')
+jake = User(username='Jake', email='jo@gmail.com', password='passowrd')
+
+db.session.add(chris)
+db.session.add(todd)
+db.session.add(anddrew)
+db.session.add(jake)
+
+db.session.commit()
